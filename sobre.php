c78281a7925a67249dcb098ac72d3e032947f9cc
Francilvan <francilvanlima@gmail.com>
2020-03-09 12:18:27 -0400

    sobre adicionado

---
+++ sobre.php
@@ -0,0 +1,15 @@
+<?php include("cabecalho.php"); ?>
+<?php include("conecta.php"); ?>
+
+<head>
+	<meta charset="utf-8">
+	<title>SOBRE</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/loja.css">
+</head>
+ <body>
+ 	<H1>Sobre nós</H1>
+	<p>Somos uma loja compromisso com o cliente e</p>
+
+
+<?php include("rodape.php"); ?>
