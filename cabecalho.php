45f8f14673f1e98c5b13ef1ccbd400dd2cebb424
Francilvan <francilvanlima@gmail.com>
2020-03-09 12:14:18 -0400

    cabecalho e conecta sal

--- cabecalho.php
+++ cabecalho.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html>
-<!DOCTYPE html>
-<html>
 <head>
 	<meta charset="utf-8">
 	<title>Minha Loja</title>
@@ -10,19 +8,20 @@
 </head>
 <body>
 
-	<div class="navbar navbar-inverse navbar-fixed-top"> <div class="container">
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
 			<div class="navbar-header">
-				<a class="navbar-brand" href="index.php">Minha loja Oficial do Francilvan</a>
+				<a class="navbar-brand" href="index.php">Minha loja</a>
 			</div>
 			<div>
 				<ul class="nav navbar-nav">
 					<li><a href="produto-formulario.php">Adiciona Produto</a></li>
-					<li><a href="sobre.php">Sobre</a></li>
-					
+					<li><a href="produto-lista.php">Produtos</a></li>
+					<li><a href="sobre.php">Sobre</a></li>	
 				</ul>
 			</div>
 		</div>
 	</div>
-
+	
 	<div class="container">
 		<div class="principal">
